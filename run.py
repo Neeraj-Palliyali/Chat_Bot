ac78341cfe96286462b0819ee70031e046add03e
palliyali.neeraj@gmail.com <palliyali.neeraj@gmail.com>
2020-06-17 11:09:58 +0530

    Bug fixes

--- run.py
+++ run.py
@@ -34,9 +34,7 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     response = str(english_bot.get_response(userText))
-    if(response=="okay" or response=="Okay"):
-        appendfile.write("Vague Reply")
-
+    
     appendfile=os.listdir('saved_conversations')[-1]
     appendfile= open('saved_conversations/'+str(filenumber),"a")
     appendfile.write('user : '+userText+'\n')
